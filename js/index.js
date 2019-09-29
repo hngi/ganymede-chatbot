43b14e2d731169d9d8a5a4a5acb1a36dbb398858
Gospel O. Chinyereugo <gospelokpara@gmail.com>
2019-09-29 08:16:45 +0100

    Chatbot updated by @Codebug

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
+
 var username = "";
 function send_message(conv,message){
 	if (conv.length > 4) {
-			conv = conv + "<br>";
+			conv = conv + "<br>"+"<div class='clear'></div>"+"<br>";
 	}
-	$("#converse").html(conv +"<span class = 'current-msg'>" + "<span id='chat-bot'>HNG Bot: </span>" + message + "</span>");
+	$("#converse").html(conv +"<span class='current-msg' style='border: 3px solid #dedede;background-color:#f1f1f1;padding:5px;border-radius:5px;display:inline-block;'>" + "<span id='chat-bot'>Ganymede Bot:"+"<span id='bot-arrow'></span>"+" </span>" + message + "</span>");
 	$(".current-msg").hide();
 	$(".current-msg").delay(500).fadeIn();
 	$(".current-msg").removeClass("current-msg");
@@ -15,8 +16,10 @@ function get_username(conv){
 
 function ai(conv,message){
 	if (username<4) {
-		username = message;
-		send_message(conv,"Hi, "+ username + ". How are you?");
+		if (message=='hi' || message=='Hi' || message=='HI') {send_message(conv,"I'm good. You didn't tell me your name.");
+			} else {
+				username = message;
+		send_message(conv,"Hi, "+ username + ". How are you?");} ;
 	}
 	else{
 		// $("#send").click(function(){
@@ -31,6 +34,29 @@ $(function(){
 	var open = false;
 	var conv = $("#converse").html();
 	get_username(conv);
+
+	$("#textbox").keypress(function (keyPress){
+		if (keyPress.keyCode==13){
+			var usermsg = $("#textbox").val().replace(/\r?\n|\r/g, "");
+		conv = $("#converse").html();
+		console.log(conv.length);
+		if (usermsg != "") {
+			$("#textbox").val("");
+			if (conv.length > 4) {
+				conv = conv + "<br>"+"<div class='clear'></div>"+"<br>";
+			}
+			$("#converse").html(conv +"<span style=''></span>"+ "<span id='chat-user'>You: "+"<span id='user-arrow'></span>"+ usermsg+"</span>");
+			$("#converse").scrollTop($("#converse").prop("scrollHeight"));
+			conv = $("#converse").html();
+			ai(conv,usermsg);
+			}
+		}
+	})
+	// function(){
+		
+	// }
+
+
 	$("#send").click(function(){
 		var usermsg = $("#textbox").val();
 		conv = $("#converse").html();
@@ -38,9 +64,9 @@ $(function(){
 		if (usermsg != "") {
 			$("#textbox").val("");
 			if (conv.length > 4) {
-				conv = conv + "<br>";
+				conv = conv + "<br>"+"<div class='clear'></div>"+"<br>";
 			}
-			$("#converse").html(conv + "<span id='chat-user'>You: </span>" + usermsg);
+			$("#converse").html(conv +"<span style=''></span>"+ "<span id='chat-user'>You: "+"<span id='user-arrow'></span>"+ usermsg+"</span>");
 			$("#converse").scrollTop($("#converse").prop("scrollHeight"));
 			conv = $("#converse").html();
 			ai(conv,usermsg);
@@ -53,3 +79,11 @@ $(function(){
 		$("#chat-box").animate({"right":"-300px"});
 	});
 });
+
+
+// document.querySelector('#text-box').addEventListener('keypress', function (keyPress){
+// 	let key = keyPress.which || keyPress.keyCode;
+// 	if (key===13){
+
+// 	}
+// })
